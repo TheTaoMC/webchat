09f8413366f4940d1582230e1271b36bbc73be20
manamanote <manamanote@gmail.com>
2024-06-10 07:18:43 +0000

    f1

---
+++ app/page.js
@@ -0,0 +1,49 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+let socket;
+
+export default function Home() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket = io();
+
+    socket.on('chat message', (msg) => {
+      setMessages((prevMessages) => [...prevMessages, msg]);
+    });
+
+    return () => {
+      socket.off('chat message');
+    };
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit('chat message', message);
+      setMessage('');
+    }
+  };
+
+  return (
+    <div>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
+      <form onSubmit={sendMessage}>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+}
